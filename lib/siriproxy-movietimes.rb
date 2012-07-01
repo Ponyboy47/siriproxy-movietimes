ac664f0b8b630bb2d726c3151e33abd3d8bb456f
Ponyboy47 <ponyboy47@gmail.com>
2012-06-30 22:31:43 -0500

    Fixed location for google_movies47 changes

--- lib/siriproxy-movietimes.rb
+++ lib/siriproxy-movietimes.rb
@@ -6,8 +6,8 @@ class SiriProxy::Plugin::MovieTimes < SiriProxy::Plugin
 def initialize(config)
 end
    listen_for /Movie times/i do
-      say "Getting movie times for Spring, Tx"#{location.city}, #{location.state}"
-      movieShowTimes = GoogleMovies47::Crawler.new({ :city => 'Spring', :state => 'Texas'})##{location.city}%2C+#{location.state}' })
+      say "Getting movie times for #{location.city}, #{location.state}"
+      movieShowTimes = GoogleMovies47::Crawler.new({ :city => '#{location.city}', :state => '#{location.state}' })
       theaters = movieShowTimes.theaters
       view1 = SiriAddViews.new
       view2 = SiriAddViews.new
