790dea6f098deca179e220dfa80d1051d049cfc1
Jacob Williams <ponyboy47@gmail.com>
2012-07-14 17:38:16 -0500

    Got sorter working

--- lib/siriproxy-movietimes.rb
+++ lib/siriproxy-movietimes.rb
@@ -6,24 +6,46 @@ class SiriProxy::Plugin::MovieTimes < SiriProxy::Plugin
 def initialize(config)
 end
 
-  filter "SetRequestOrigin", direction: :from_iphone do |object|
-    puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
-  end
-  
-  def getTheaters(film)
-    theaters = Array.new
-    film.each do |f|
-      theaters << f[1][:cinema][:name]
+#  filter "SetRequestOrigin", direction: :from_iphone do |object|
+#    puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
+#  end
+#12
+  def organizeFilmsByTheater(film)
+    theaters = Hash.new
+    movies = Hash.new
+    theaternames = []
+    theaterinfo = []
+    w = 0
+    x = 0
+    y = 0
+    z = 0
+
+      while w < film[1].length do
+      if theaterinfo.include?(film[1][w][:cinema][:name]) == false
+        theaterinfo = []
+        theaterinfo << film[1][w][:cinema][:name]
+        theaterinfo << film[1][w][:cinema][:address]
+        theaterinfo << film[1][w][:cinema][:phone]
+        theaternames << theaterinfo
+      end
+      w = w + 1
+    end
+    while x < theaternames.length do
+      while y < film[1].length do
+        showtimes = []
+        while z < film[1][y][:showtimes].length do
+          showtimes << film[1][y][:showtimes][z][:time]
+          z = z + 1
+        end
+        movies[y] = { :name => film[1][y][:film][:name], :times => showtimes } if showtimes.length > 0
+        y = y + 1
+      end
+      theaters[x] = { :info => { :name => theaternames[x][0], :address => theaternames[x][1], :phone => theaternames[x][2] }, :movies => movies }
+      x = x + 1
     end
     return theaters
   end
-  def organizeFilmsByTheater(films)
-    theaters = [][]
-    films.each do |f|
-      theaters[f[1][:cinema][:name]] << f[1][:film][:name]
-    end
-  end
-  
+
   listen_for /Movie time(?:s)?/i do
      if location.country == "United States"
        say "Getting movie times for #{location.city}, #{location.state}"
@@ -32,9 +54,9 @@ end
        say "Getting movie times for #{location.city}, #{location.country}"
        movies = GoogleShowtimes.for("#{location.city}%2C+#{location.country}")
      end
-     #theaters = getTheaters(movies)
-     puts "FIRST THING #{movies[1][1][0]}"
-     puts "SECOND THING #{movies[2]}"
+     theaters = organizeFilmsByTheater(movies)
+     puts theaters
      request_completed
   end
 end
+
