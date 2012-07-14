66fb04f3806454f5e0a4cce5607fa7dfdf366c02
Jacob Williams <ponyboy47@gmail.com>
2012-07-14 11:50:14 -0500

    debugging

--- lib/siriproxy-movietimes.rb
+++ lib/siriproxy-movietimes.rb
@@ -23,50 +23,19 @@ end
       view.make_root(last_ref_id)
       view.scrollToTop = true
       movieTimesLines1 = Array.new
-      movieTimesLines2 = Array.new
-      movieTimesLines3 = Array.new
 
       movies1 = theaters[0].movies
       x = 0
+      puts "#{movies1.length}"
       until x == (movies1.length - 1)
-         movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].name}")
-         movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].times}")
-         x = x+1
+        puts "#{movies1[x].name}"
+        puts "#{movies1[x].times}"
+        movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].name}")
+        movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].times}")
+        x = x+1
       end
       movieTimesList1 = SiriAnswer.new("#{theaters[0].name}", [movieTimesLines1])
       view.views << SiriAnswerSnippet.new([movieTimesList1])
-
-#      movies2 = theaters[1].movies
-#      x = 0
-#      until x == (movies2.count - 1)
-#         y = 0
-#         movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].name}")
-#         puts "#{movies2[x].name}"
-#         until y == (movies2[x].times.count - 1)
-#            movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].times[y]}")
-#            puts "#{movies2[x].times[y]}"
-#            y = y + 1
-#         end
-#         x = x+1
-#      end
-#      movieTimesList2 = SiriAnswer.new("#{theaters[1].name}", [movieTimesLines2])
-#      view.views << SiriAnswerSnippet.new([movieTimesList2])
-#      
-#      movies3 = theaters[2].movies
-#      x = 0
-#      until x == (movies3.count - 1)
-#         y = 0
-#         movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].name}")
-#         puts "#{movies3[x].name}"
-#         until y == (movies3[x].times.count - 1)
-#            movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].times[y]}")
-#            puts "#{movies3[x].times[y]}"
-#            y = y + 1
-#         end
-#         x = x+1
-#      end
-#      movieTimesList3 = SiriAnswer.new("#{theaters[2].name}", [movieTimesLines3])
-#      view.views << SiriAnswerSnippet.new([movieTimesList3])
       
       send_object view
       
