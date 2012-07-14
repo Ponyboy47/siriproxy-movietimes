c2f3a44bb62d9536cf834824521fbecc29c8c1d3
Jacob Williams <ponyboy47@gmail.com>
2012-07-14 12:26:57 -0500

    added more theaters

--- lib/siriproxy-movietimes.rb
+++ lib/siriproxy-movietimes.rb
@@ -9,6 +9,45 @@ end
    filter "SetRequestOrigin", direction: :from_iphone do |object|
      puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
    end
+  def getEverything(@theaters,start)
+    sleep 3
+    view = SiriAddViews.new
+    view.make_root(last_ref_id)
+    view.scrollToTop = true
+    movieTimesLines1 = []
+    movieTimesLines2 = []
+    movieTimesLines3 = []
+  
+    movies1 = @theaters[start].movies
+    x = 0
+    until x == (movies1.length - 1)
+      movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].name}")
+      movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].times}")
+      x = x+1
+    end
+    movieTimesList1 = SiriAnswer.new("#{@theaters[start].name}", movieTimesLines1)
+    
+    movies2 = @theaters[start+1].movies
+    x = 0
+    until x == (movies2.length - 1)
+      movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].name}")
+      movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].times}")
+      x = x+1
+    end
+    movieTimesList2 = SiriAnswer.new("#{@theaters[start+1].name}", movieTimesLines2)
+    
+    movies3 = @theaters[start+2].movies
+    x = 0
+    until x == (movies3.length - 1)
+      movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].name}")
+      movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].times}")
+      x = x+1
+    end
+    movieTimesList3 = SiriAnswer.new("#{@theaters[start+2].name}", movieTimesLines3)
+    view.views << SiriAnswerSnippet.new([movieTimesList1,movieTimesList2,movieTimesList3])
+    
+    return view
+  end
 
    listen_for /Movie times/i do
       if location.country == "United States"
@@ -18,47 +57,17 @@ end
         say "Getting movie times for #{location.city}, #{location.country}"
         movieShowTimes = GoogleMovies::Client.new("#{location.city}%2C+#{location.country}")
       end
-      @theaters = movieShowTimes.movies_theaters
-      view = SiriAddViews.new
-      view.make_root(last_ref_id)
-      view.scrollToTop = true
-      movieTimesLines1 = []
-      movieTimesLines2 = []
-      movieTimesLines3 = []
-
-      movies1 = @theaters[0].movies
-      x = 0
-      puts "#{movies1.length}"
-      until x == (movies1.length - 1)
-        movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].name}")
-        movieTimesLines1 << SiriAnswerLine.new("#{movies1[x].times}")
-        x = x+1
+      theaters = movieShowTimes.movies_theaters
+      shows = getEverything(theaters,0)
+      
+      send_object shows
+      more = confirm "Would you like to see more theaters?"
+      if more
+        shows1 = getEverything(theaters,3)
+        send_object shows1
+      else
+        say "Have a nice day."
       end
-      movieTimesList1 = SiriAnswer.new("#{@theaters[0].name}", movieTimesLines1)
-      
-      movies2 = @theaters[1].movies
-      x = 0
-      puts "#{movies2.length}"
-      until x == (movies2.length - 1)
-        movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].name}")
-        movieTimesLines2 << SiriAnswerLine.new("#{movies2[x].times}")
-        x = x+1
-      end
-      movieTimesList2 = SiriAnswer.new("#{@theaters[1].name}", movieTimesLines2)
-      
-      movies3 = @theaters[2].movies
-      x = 0
-      puts "#{movies3.length}"
-      until x == (movies3.length - 1)
-        movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].name}")
-        movieTimesLines3 << SiriAnswerLine.new("#{movies3[x].times}")
-        x = x+1
-      end
-      movieTimesList3 = SiriAnswer.new("#{@theaters[2].name}", movieTimesLines3)
-      view.views << SiriAnswerSnippet.new([movieTimesList1,movieTimesList2,movieTimesList3])
-      
-      send_object view
-      
       request_completed
    end
 end
